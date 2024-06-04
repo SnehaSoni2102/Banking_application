880fd3aa2953078a1aa1ce0f9a28eb267a50739f
Sneha Soni <snehasoni@Snehas-MacBook-Air.local>
2024-06-04 13:48:35 +0530

    Deposite RESTAPI created

--- src/main/java/banking_application/service/AccountService.java
+++ src/main/java/banking_application/service/AccountService.java
@@ -5,4 +5,6 @@ import banking_application.dto.AccountDto;
 public interface AccountService {
     AccountDto createAccount(AccountDto account);
     AccountDto getAccountById(Long id);
+
+    AccountDto deposit(Long id , double amount);
 }
